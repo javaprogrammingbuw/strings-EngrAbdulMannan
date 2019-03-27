985eb878d98c0e0e4c6f66cee735bc4027b7d9d3
abdulmannan <abdulmannancc33@gmail.com>
2019-03-27 17:21:03 +0100

    string done

--- Expand.java
+++ Expand.java
@@ -3,41 +3,65 @@ import java.util.Scanner;
 //advanced
 
 public class Expand {
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter a word first number the alphabet ");
+		System.out.print("Enter the input ");
 		String n =scan.nextLine();
-		System.out.println(n);
-		System.out.println("result is"+doPrint(n));
-		System.out.println("result is"+intToString(n));
-		
-		//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
-		//expand the String by printing each letter so many times as the number before the letter indicates
-		//e.g. 2a3b5c -> aabbbccccc
-		//Hint: first start with inputs where there is always a number and then a character, then think about how
-		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
-		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
-		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
-		
+		//System.out.println(n);
+		exp(n);
+
 	}
-	public static char doPrint(String a) {
-		for (int i=0;i<a.length()-1;i=i+2) {
-			System.out.println(a.charAt(i));
-		
-		}
-		return 0;
-		
+	//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
+	//expand the String by printing each letter so many times as the number before the letter indicates
+	//e.g. 2a3b5c -> aabbbccccc
+	//Hint: first start with inputs where there is always a number and then a character, then think about how
+	//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
+	//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
+	//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
+
+	public static void exp(String a){
+
+		String b="0";
+
+		for(int i=0;i<a.length();i++){
+
+			char c=a.charAt(i);
+
+			if(Character.isDigit(c)){
+
+				b+=c;
+
+			}else{
+
+
+
+				int n=Integer.parseInt(b);
+
+				if(n==0){
+
+					System.out.print(c);
+
+				}else{
+
+					for(int j=0;j<n;j++){
+
+						System.out.print(c);
+
+					}
+
+				}
+
+
+
+				b="0";
+
+			}
+
+		} 
+
+
+
 	}
-	public static char intToString(String b) {
-		for (int i=1;i<b.length();i=i+2) {
-			//for (int j=1;j<b.length();j=j+2) {
-				char c=b.charAt(i);
-				//c=doPrint(b);
-				
-		//	}
-			
-		}
-		return c;
-	}
+
 }
--- Palindrome.java
+++ Palindrome.java
@@ -7,30 +7,39 @@ public class Palindrome{
 		System.out.print("Enter a name ");
 		String n =scan.nextLine();
 		System.out.println("The input is palindrome"+isPalindrome(n));
-	//todo: Create a method isPalindrome which takes a String as input and returns true if it is a palindrome and false otherwise.
-	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
-	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
-	//Ignore also spaces. 'Taco cat' is a valid palindrome.
+		//todo: Create a method isPalindrome which takes a String as input and returns true if it is a palindrome and false otherwise.
+		//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
+		//Ignore upper and lower case. 'Hannah' is a palindrome, too.
+		//Ignore also spaces. 'Taco cat' is a valid palindrome.
 
-}
-	public static boolean isPalindrome(String a) {
-		a.replace("\\s", "");
-		for (int i=0;i<a.length()/2;i++) {
-			if(a.charAt(i)!=a.charAt(a.length()-1-i)   ){
+	}
+	public static boolean isPalindrome(String a0) {
+		//a0.replace("\\s", "");
+		String b="";
+		String a=a0.toLowerCase();
+		for (int j=0;j<a.length();j++) {
+			if(!Character.isWhitespace(a.charAt(j))) {
+				b+=a.charAt(j);
+			}
+
+		}
+
+		for (int i=0;i<b.length()/2;i++) {
+			if(a.charAt(i)!=b.charAt(b.length()-1-i)   ){
 				//&& Character.isUpperCase(a.charAt(i)) && Character.isLowerCase(a.charAt(i))
 				//System.out.println(i + ", " + a.charAt(i) + ", " + (a.length()-1-i) + ", " + a.charAt(a.length()-1-i));
 				return false;
 			}
-			else if (Character.isUpperCase(a.charAt(i))&&Character.isLowerCase(a.charAt(i))) {
-				isPalindrome(a);
+			else if (Character.isUpperCase(b.charAt(i))&&Character.isLowerCase(b.charAt(i))) {
+				isPalindrome(b);
 			}
-			else if(Character.isSpaceChar(a.charAt(i))) {
-				isPalindrome(a);
+			else if(Character.isSpaceChar(b.charAt(i))) {
+				isPalindrome(b);
 			}
-			
-	
-		
-			
+
+
+
+
 		}
 		return true;
 	}
